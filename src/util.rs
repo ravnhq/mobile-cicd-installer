7185180eb884127619479f69d6070b9cef86bc97
Kevin DC <kevindelcastillo@ravn.co>
2024-01-03 09:36:57 -0500

    feat: Implement `copy_recursively` util function

--- src/util.rs
+++ src/util.rs
@@ -6,3 +6,27 @@ macro_rules! log {
         println!("{prompt} {}", format!($($arg)*));
     }};
 }
+
+pub mod fs {
+    use std::path::Path;
+    use anyhow::Result;
+    use fs_extra::dir::CopyOptions;
+
+    pub fn copy_recursively(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> Result<()> {
+        let options = CopyOptions::new()
+            .overwrite(true)
+            .copy_inside(true);
+
+        let dst = dst.as_ref();
+        if dst.exists() {
+            if dst.is_dir() {
+                std::fs::remove_dir_all(dst)?;
+            } else {
+                std::fs::remove_file(dst)?;
+            }
+        }
+
+        fs_extra::dir::copy(src, dst, &options)?;
+        Ok(())
+    }
+}
